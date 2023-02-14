8b217bf8548de5b3d83f44d6018c4b5370e4757e
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-02-14 09:23:04 +0100

    changed structure, start styling

--- courses/src/app/app-routing.module.ts
+++ courses/src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ForMyselfComponent } from './pages/for-myself/for-myself.component';
 import { ForVisagisteComponent } from './pages/for-visagiste/for-visagiste.component';
-import { MainComponent } from './pages/main/main.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    component: MainComponent,
-  },
-  {
-    path: 'for-myself',
-    pathMatch: 'full',
-    component: ForMyselfComponent,
-  },
-  {
-    path: 'for-visagiste',
     pathMatch: 'full',
     component: ForVisagisteComponent,
   },
--- courses/src/app/app.module.ts
+++ courses/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ForMyselfComponent } from './pages/for-myself/for-myself.component';
 import { ForVisagisteComponent } from './pages/for-visagiste/for-visagiste.component';
-import { MainComponent } from './pages/main/main.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    ForMyselfComponent,
-    ForVisagisteComponent,
-    MainComponent,
-  ],
+  declarations: [AppComponent, PageNotFoundComponent, ForVisagisteComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
