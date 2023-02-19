b2d593470a0a7fad785cb579b596ae38a4e1ab32
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-02-19 18:03:08 +0100

    another slider installed

--- courses/src/app/app.module.ts
+++ courses/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForVisagisteComponent } from './pages/for-visagiste/for-visagiste.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent, ForVisagisteComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MdbCarouselModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- courses/src/app/pages/for-visagiste/for-visagiste.component.ts
+++ courses/src/app/pages/for-visagiste/for-visagiste.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-for-visagiste',
   templateUrl: './for-visagiste.component.html',
-  styleUrls: ['./for-visagiste.component.scss']
+  styleUrls: ['./for-visagiste.component.scss'],
 })
 export class ForVisagisteComponent {
-
+  imageObject: Array<object> = [
+    {
+      image: 'assets/img/slider/circle.JPG',
+      thumbImage: 'assets/img/slider/circle.JPG',
+    },
+    {
+      image: 'assets/img/slider/circle.JPG',
+      thumbImage: 'assets/img/slider/circle.JPG',
+    },
+    {
+      image: 'assets/img/slider/circle.JPG',
+      thumbImage: 'assets/img/slider/circle.JPG',
+    },
+    {
+      image: 'assets/img/slider/circle.JPG',
+      thumbImage: 'assets/img/slider/circle.JPG',
+    },
+    {
+      image: 'assets/img/slider/circle.JPG',
+      thumbImage: 'assets/img/slider/circle.JPG',
+    },
+    {
+      image:
+        'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+      thumbImage:
+        'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+    },
+    {
+      video: 'https://youtu.be/6pxRHBw-k8M',
+    },
+  ];
 }
