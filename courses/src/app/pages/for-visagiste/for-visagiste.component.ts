301bcda5d5659fffb3868108d83fe516efea1b8d
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-02-19 20:01:55 +0100

    slider working, added new imgs

--- courses/src/app/pages/for-visagiste/for-visagiste.component.ts
+++ courses/src/app/pages/for-visagiste/for-visagiste.component.ts
@@ -5,36 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './for-visagiste.component.html',
   styleUrls: ['./for-visagiste.component.scss'],
 })
-export class ForVisagisteComponent {
-  imageObject: Array<object> = [
-    {
-      image: 'assets/img/slider/circle.JPG',
-      thumbImage: 'assets/img/slider/circle.JPG',
-    },
-    {
-      image: 'assets/img/slider/circle.JPG',
-      thumbImage: 'assets/img/slider/circle.JPG',
-    },
-    {
-      image: 'assets/img/slider/circle.JPG',
-      thumbImage: 'assets/img/slider/circle.JPG',
-    },
-    {
-      image: 'assets/img/slider/circle.JPG',
-      thumbImage: 'assets/img/slider/circle.JPG',
-    },
-    {
-      image: 'assets/img/slider/circle.JPG',
-      thumbImage: 'assets/img/slider/circle.JPG',
-    },
-    {
-      image:
-        'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-      thumbImage:
-        'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    },
-    {
-      video: 'https://youtu.be/6pxRHBw-k8M',
-    },
-  ];
-}
+export class ForVisagisteComponent {}
